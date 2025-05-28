fd73787e6a0156e0a564ede3257c3f119d33aeac
Maelito75 <maelcervo@yahoo.fr>
2025-05-28 13:12:26 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const checkbox = document.querySelector('.hamburger-menu input');
   const logo = document.getElementById('logo-img');
   const texte_bienvenue = document.getElementById('texte-bienvenue');
+  const Bienvenue = document.querySelector('.Bienvenue');
   const content = document.querySelector('.content');
   const dessin = document.querySelector('.dessin-tri');
   const timer = document.querySelector('.Timer');
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     logo?.classList.toggle('menu-open', isChecked);
     texte_bienvenue?.classList.toggle('menu-open', isChecked);
+    Bienvenue?.classList.toggle('menu-open', isChecked);
     content?.classList.toggle('menu-open', isChecked);
     dessin?.classList.toggle('menu-open', isChecked);
     timer?.classList.toggle('menu-open', isChecked);
